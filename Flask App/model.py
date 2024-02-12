76f72be9c8a6efb7b71c265cde64116775ed9e5c
Ryan Gillespie <ryan_p_gillespie@yahoo.com>
2024-02-12 12:34:24 -0500

    put model into function to be used in Flask

---
+++ Flask App/model.py
@@ -0,0 +1,7 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+def check_phishing(text):
+    pipe = pipeline("text-classification", model="foghlaimeoir/phishing-DistilBERT")
+    res = pipe(text)
+    print(res)
--- model/prototype_model2.py
+++ model/prototype_model2.py
@@ -1,6 +1,7 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
+
 pipe = pipeline("text-classification", model="foghlaimeoir/phishing-DistilBERT")
 
 res = pipe("Hey buddy how's it going?")
