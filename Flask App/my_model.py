288d1f057c2e4cf54ebf7e75b5ef69542b7b4671
rpg94 <147991652+rpg94@users.noreply.github.com>
2024-03-05 12:01:14 -0500

    Update my_model.py

--- Flask App/my_model.py
+++ Flask App/my_model.py
@@ -2,6 +2,6 @@
 from transformers import pipeline
 
 def check_phishing(text):
-    pipe = pipeline("text-classification", model="rpg1/tinyBERT_phishing_model")
+    pipe = pipeline("text-classification", model="rpg1/tinyBERT_phishing_model", truncation=True, max_length=512)
     res = pipe(text)
     return res
