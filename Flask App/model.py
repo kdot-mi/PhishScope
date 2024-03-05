08849d0fd0c965471fe78d310b6ac87515ffa00f
rpg94 <147991652+rpg94@users.noreply.github.com>
2024-03-05 12:02:20 -0500

    Delete Flask App/model.py
    Cleanup, going to use my_model.py

--- Flask App/model.py
+++
@@ -1,7 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-def check_phishing(text):
-    pipe = pipeline("text-classification", model="foghlaimeoir/phishing-DistilBERT", truncation=True, max_length=512)
-    res = pipe(text)
-    return res
